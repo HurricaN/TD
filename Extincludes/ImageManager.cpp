fbbd9d475f7af5b1b445f587c994048a998cdcc2
Nicolas Ferreira <nicolas94ferreira@yahoo.fr>
2012-07-05 15:46:43 +0200

    no message

--- Extincludes/ImageManager.cpp
+++ Extincludes/ImageManager.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 sf::Image* ImageManager::load( const std::string& strId ) {
 	sf::Image* image = new sf::Image();
-	
-	// caca
+
 	if( !image->LoadFromFile( strId ) ) {
 		cout << "[WARN] ImageManager failed to load: " << strId << endl;
 		delete image;
