664d4d4ee826c82f32a56f63f39d63b25c90a64d
Tristan Sterin <trs421@free.fr>
2012-07-09 20:23:49 +0200

    Avec c'est mieux !

---
+++ Mob/mobAttributes.h
@@ -0,0 +1,18 @@
+/*
+ *  mobAttributes.h
+ *  TD-XCode
+ *
+ *  Created by Tristan Stérin on 08/07/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef DEF_MOBATT_H
+#define DEF_MOBATT_H
+
+struct mobAttributes
+{
+	int velocity;
+};
+
+#endif
